f7ae7777483abc3398206aff89916809b709db15
Sebastian Hyland <st.hyland05@gmail.com>
2025-06-29 04:19:00 -0700

    feat(dsl): implemented paths! macro

--- crates/dsl/src/macros.rs
+++ crates/dsl/src/macros.rs
@@ -1,1 +1,24 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{LitStr, parse::Parse, parse_macro_input, punctuated::Punctuated, token::Comma};
 
+struct MacroLits(Punctuated<LitStr, Comma>);
+impl Parse for MacroLits {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        Ok(MacroLits(Punctuated::parse_terminated(input)?))
+    }
+}
+
+#[proc_macro]
+pub fn paths(input: TokenStream) -> TokenStream {
+    let MacroLits(path_lits) = parse_macro_input!(input as MacroLits);
+    let path_lits = path_lits.iter();
+    quote! {
+        vec![
+            #(
+                ::std::path::PathBuf::from(#path_lits)
+            ),*
+        ]
+    }
+    .into()
+}

cd6d5c9d45bcc0c0ec95fde6393f40abe6562323
Sebastian Hyland <st.hyland05@gmail.com>
2025-09-20 16:21:23 -0700

    feat(lib): support MAESTRO_SESSION_ID env var

--- lib/src/session.rs
+++ lib/src/session.rs
@@ -13,7 +13,7 @@ use std::{
 pub(crate) static SESSION_WORKDIR: OnceLock<PathBuf> = OnceLock::new();
 
 pub(crate) fn setup_session_workdir() -> Result<PathBuf, io::Error> {
-    let session_id = {
+    let session_id = env::var("MAESTRO_SESSION_ID").unwrap_or_else(|_| {
         let mut rng = rand::rng();
         let selected_adj = ADJECTIVES
             .choose(&mut rng)
@@ -22,13 +22,31 @@ pub(crate) fn setup_session_workdir() -> Result<PathBuf, io::Error> {
             .choose(&mut rng)
             .expect("ANIMALS should not be empty for session ID selection!");
         format!("{}-{}", selected_adj, selected_animal)
-    };
+    });
     let maestro_workdir = match env::var("MAESTRO_WORKDIR") {
         Ok(v) => PathBuf::from(v),
         Err(_) => env::current_dir()?.join("maestro_work"),
     };
     let session_workdir = maestro_workdir.join(&session_id);
     fs::create_dir_all(&session_workdir)?;
+
+    // Check if there are any existing directories in the session working directory
+    if fs::read_dir(&session_workdir)?.any(|item| {
+        if let Ok(entry) = item
+            && let Ok(ty) = entry.file_type()
+            && ty.is_dir()
+        {
+            true
+        } else {
+            false
+        }
+    }) {
+        return Err(io::Error::new(
+            io::ErrorKind::DirectoryNotEmpty,
+            format!("Directory {} is not empty!", session_workdir.display()),
+        ));
+    }
+
     fs::write(
         session_workdir.join(".maestro.active"),
         process::id().to_string(),

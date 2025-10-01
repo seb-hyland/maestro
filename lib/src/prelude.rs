b086cf4c56528b73d4f438d02394d628c59d4a07
Sebastian Hyland <st.hyland05@gmail.com>
2025-09-30 19:24:51 -0700

    chore(lib): update prelude

--- lib/src/prelude.rs
+++ lib/src/prelude.rs
@@ -2,6 +2,7 @@ pub use crate::{IntoArray, WorkflowResult, arg, inputs};
 pub use dagger::{
     dagger,
     parallelize::{parallelize, parallelize_with_time_limit},
+    result::{NodeError, NodeResult},
 };
 pub use maestro_macros::process;
 pub use std::{
